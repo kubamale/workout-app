b7963643a09e451404fd722a3e228f4a2703a965
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-02-06 11:01:00 +0100

    Statistics service initialisation

---
+++ statistics-service/src/main/kotlin/malewicz/jakub/statistics_service/StatisticsServiceApplication.kt
@@ -0,0 +1,11 @@
+package malewicz.jakub.statistics_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StatisticsServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<StatisticsServiceApplication>(*args)
+}
---
+++ statistics-service/src/test/kotlin/malewicz/jakub/statistics_service/StatisticsServiceApplicationTests.kt
@@ -0,0 +1,15 @@
+package malewicz.jakub.statistics_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+class StatisticsServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
