4a47ee4c341c9bfb344c842f85ea3dcbd09cf021
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-02-12 15:31:19 +0100

    Remove exercise entity from workout service

--- workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/dtos/WorkoutExerciseDetails.kt
+++ workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/dtos/WorkoutExerciseDetails.kt
@@ -1,12 +1,12 @@
 package malewicz.jakub.workout_service.workout.dtos
 
+import java.util.*
 import malewicz.jakub.workout_service.exercise.dtos.ExerciseDetails
 import malewicz.jakub.workout_service.set.dtos.SetDetailsDto
-import java.util.*
 
 data class WorkoutExerciseDetails(
     val id: UUID,
-    val exercise: ExerciseDetails,
+    val exercise: ExerciseDetails?,
     val order: Int,
     val sets: MutableList<SetDetailsDto>,
 )
--- workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/entities/WorkoutExerciseEntity.kt
+++ workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/entities/WorkoutExerciseEntity.kt
@@ -1,27 +1,22 @@
 package malewicz.jakub.workout_service.workout.entities
 
 import jakarta.persistence.*
-import malewicz.jakub.workout_service.exercise.entities.ExerciseEntity
-import malewicz.jakub.workout_service.set.entities.SetEntity
 import java.util.*
+import malewicz.jakub.workout_service.set.entities.SetEntity
 
 @Entity(name = "workout_exercises")
 class WorkoutExerciseEntity(
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
-    var id: UUID? = null,
-    @ManyToOne(fetch = FetchType.LAZY)
-    var workout: WorkoutEntity,
-    @ManyToOne(fetch = FetchType.LAZY)
-    var exercise: ExerciseEntity,
+    @Id @GeneratedValue(strategy = GenerationType.UUID) var id: UUID? = null,
+    @ManyToOne(fetch = FetchType.LAZY) var workout: WorkoutEntity,
+    var exerciseId: UUID,
     @OneToMany(mappedBy = "workoutExercise", cascade = [CascadeType.ALL], orphanRemoval = true)
     var sets: MutableList<SetEntity> = mutableListOf(),
     var exerciseOrder: Int
 ) {
-    constructor(workout: WorkoutEntity, exercise: ExerciseEntity, sets: MutableList<SetEntity>, order: Int) : this(
-        null,
-        workout,
-        exercise,
-        sets,
-        order
-    )
+  constructor(
+      workout: WorkoutEntity,
+      exerciseId: UUID,
+      sets: MutableList<SetEntity>,
+      order: Int
+  ) : this(null, workout, exerciseId, sets, order)
 }
