23fc6f3c7d44414a50c268b1bc3197884ff2e228
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-02-12 09:39:22 +0100

    initialize exercise service

---
+++ exercise-service/src/main/kotlin/malewicz/jakub/exercise_service/ExerciseServiceApplication.kt
@@ -0,0 +1,11 @@
+package malewicz.jakub.exercise_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExerciseServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExerciseServiceApplication>(*args)
+}
---
+++ exercise-service/src/test/kotlin/malewicz/jakub/exercise_service/ExerciseServiceApplicationTests.kt
@@ -0,0 +1,15 @@
+package malewicz.jakub.exercise_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ExerciseServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
