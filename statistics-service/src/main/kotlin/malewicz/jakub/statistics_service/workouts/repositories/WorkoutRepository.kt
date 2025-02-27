816435519ed6c54485c614cc37439589faa8fc2b
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-02-27 13:28:46 +0100

    get days since last workout

--- statistics-service/src/main/kotlin/malewicz/jakub/statistics_service/workouts/repositories/WorkoutRepository.kt
+++ statistics-service/src/main/kotlin/malewicz/jakub/statistics_service/workouts/repositories/WorkoutRepository.kt
@@ -2,7 +2,14 @@ package malewicz.jakub.statistics_service.workouts.repositories
 
 import malewicz.jakub.statistics_service.workouts.entities.UserWorkoutEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDateTime
+import java.util.*
 
 interface WorkoutRepository : JpaRepository<UserWorkoutEntity, UUID> {
+
+  @Query("SELECT uw.date FROM user_workouts uw WHERE uw.userId = :userId ORDER BY uw.date DESC LIMIT 1")
+  fun getLatestWorkoutDate(userId: UUID): LocalDateTime?
+
+  fun getAllByUserIdAndDateGreaterThan(userId: UUID, date: LocalDateTime): List<UserWorkoutEntity>
 }
--- statistics-service/src/main/kotlin/malewicz/jakub/statistics_service/workouts/services/WorkoutService.kt
+++ statistics-service/src/main/kotlin/malewicz/jakub/statistics_service/workouts/services/WorkoutService.kt
@@ -4,12 +4,24 @@ import malewicz.jakub.statistics_service.workouts.dtos.WorkoutCreateRequest
 import malewicz.jakub.statistics_service.workouts.mappers.WorkoutMapper
 import malewicz.jakub.statistics_service.workouts.repositories.WorkoutRepository
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 @Service
 class WorkoutService(private val workoutRepository: WorkoutRepository, private val workoutMapper: WorkoutMapper) {
-    fun createWorkout(workoutCreateRequest: WorkoutCreateRequest, userId: UUID) {
-        val workout = workoutMapper.toUserWorkoutEntity(workoutCreateRequest, userId)
-        workoutRepository.save(workout)
-    }
+  fun createWorkout(workoutCreateRequest: WorkoutCreateRequest, userId: UUID) {
+    val workout = workoutMapper.toUserWorkoutEntity(workoutCreateRequest, userId)
+    workoutRepository.save(workout)
+  }
+
+  fun getDaysSinceLastWorkout(userId: UUID) =
+    workoutRepository.getLatestWorkoutDate(userId)?.let { ChronoUnit.DAYS.between(LocalDate.now(), it) }
+
+  fun getWorkoutsFromCurrentWeek(userId: UUID) {
+    val today = LocalDate.now()
+    val dayOfWeek = today.getDayOfWeek() - 1
+    val weekStart = today.minusDays(dayOfWeek.value.toLong())
+    val usersWorkouts = workoutRepository.getAllByUserIdAndDateGreaterThan(userId, weekStart.atTime(0,0,0))
+  }
 }
