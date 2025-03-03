c9f9a9743760239b08f2e2cf149a2f3a1ef99000
Jakub Malewicz <kubamalewicz002@gmail.com>
2025-03-03 14:39:09 +0100

    chore: make workout exercises type specific (DISTANCE, TIME, WEIGHT)

--- workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/entities/WorkoutExerciseEntity.kt
+++ workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/entities/WorkoutExerciseEntity.kt
@@ -1,22 +1,90 @@
 package malewicz.jakub.workout_service.workout.entities
 
 import jakarta.persistence.*
+import malewicz.jakub.workout_service.set.entities.DistanceSetEntity
 import java.util.*
-import malewicz.jakub.workout_service.set.entities.SetEntity
+import malewicz.jakub.workout_service.set.entities.TimeSetEntity
+import malewicz.jakub.workout_service.set.entities.WeightSetEntity
 
 @Entity(name = "workout_exercises")
-class WorkoutExerciseEntity(
-    @Id @GeneratedValue(strategy = GenerationType.UUID) var id: UUID? = null,
-    @ManyToOne(fetch = FetchType.LAZY) var workout: WorkoutEntity,
-    var exerciseId: UUID,
-    @OneToMany(mappedBy = "workoutExercise", cascade = [CascadeType.ALL], orphanRemoval = true)
-    var sets: MutableList<SetEntity> = mutableListOf(),
-    var exerciseOrder: Int
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+abstract class WorkoutExerciseEntity(
+  @Id @GeneratedValue(strategy = GenerationType.UUID) var id: UUID? = null,
+  @ManyToOne(fetch = FetchType.LAZY) var workout: WorkoutEntity,
+  var exerciseId: UUID,
+  var exerciseOrder: Int,
 ) {
   constructor(
-      workout: WorkoutEntity,
-      exerciseId: UUID,
-      sets: MutableList<SetEntity>,
-      order: Int
-  ) : this(null, workout, exerciseId, sets, order)
+    workout: WorkoutEntity,
+    exerciseId: UUID,
+    order: Int,
+  ) : this(null, workout, exerciseId, order)
+
+}
+
+@Entity
+class WeightWorkoutExerciseEntity(
+  id: UUID? = null,
+  workout: WorkoutEntity,
+  exerciseId: UUID,
+  order: Int,
+  @OneToMany(mappedBy = "workoutExercise", cascade = [CascadeType.ALL], orphanRemoval = true)
+  var sets: MutableList<WeightSetEntity> = mutableListOf(),
+) : WorkoutExerciseEntity(
+  id,
+  workout,
+  exerciseId,
+  order,
+) {
+  constructor(
+    workout: WorkoutEntity,
+    exerciseId: UUID,
+    order: Int,
+    sets: MutableList<WeightSetEntity> = mutableListOf()
+  ) : this(null, workout, exerciseId, order, sets)
+
+}
+
+@Entity
+class TimeWorkoutExerciseEntity(
+  id: UUID? = null,
+  workout: WorkoutEntity,
+  exerciseId: UUID,
+  order: Int,
+  @OneToMany(mappedBy = "workoutExercise", cascade = [CascadeType.ALL], orphanRemoval = true)
+  var sets: MutableList<TimeSetEntity> = mutableListOf(),
+) : WorkoutExerciseEntity(
+  id,
+  workout,
+  exerciseId,
+  order,
+) {
+  constructor(
+    workout: WorkoutEntity,
+    exerciseId: UUID,
+    order: Int,
+    sets: MutableList<TimeSetEntity> = mutableListOf()
+  ) : this(null, workout, exerciseId, order, sets)
+}
+
+@Entity
+class DistanceWorkoutExerciseEntity(
+  id: UUID? = null,
+  workout: WorkoutEntity,
+  exerciseId: UUID,
+  order: Int,
+  @OneToMany(mappedBy = "workoutExercise", cascade = [CascadeType.ALL], orphanRemoval = true)
+  var sets: MutableList<DistanceSetEntity> = mutableListOf(),
+) : WorkoutExerciseEntity(
+  id,
+  workout,
+  exerciseId,
+  order,
+) {
+  constructor(
+    workout: WorkoutEntity,
+    exerciseId: UUID,
+    order: Int,
+    sets: MutableList<DistanceSetEntity> = mutableListOf()
+  ) : this(null, workout, exerciseId, order, sets)
 }
--- workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/repositories/WorkoutExerciseRepository.kt
+++ workout-service/src/main/kotlin/malewicz/jakub/workout_service/workout/repositories/WorkoutExerciseRepository.kt
@@ -1,14 +1,9 @@
 package malewicz.jakub.workout_service.workout.repositories
 
-import java.util.*
 import malewicz.jakub.workout_service.workout.entities.WorkoutExerciseEntity
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface WorkoutExerciseRepository : JpaRepository<WorkoutExerciseEntity, UUID> {
-
-  @EntityGraph(attributePaths = ["sets"])
-  fun findByWorkoutId(workoutId: UUID): List<WorkoutExerciseEntity>
-
   fun findByIdAndWorkoutId(id: UUID, workoutId: UUID): Optional<WorkoutExerciseEntity>
 }
